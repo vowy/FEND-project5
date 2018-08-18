c5dae69cb04abb66bed8b1d6aa00dbe00d6a6783
Eric Parsons <e.b.parsons@gmail.com>
2018-08-18 18:13:51 -0500

    Update caching with Service Worker.

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
+const cacheVersion = 'v1';
+
+const cachedPages = [
+  'index.html',
+  'restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+];
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker Installed');
+
+  event.waitUntil(
+    caches.open(cacheVersion).then(cache => {
+      cache.addAll(cachedPages);
+    })
+     .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
