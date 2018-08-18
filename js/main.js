dd77921df22fa5dccadc1ab2587e3997a49ffb9a
Eric Parsons <e.b.parsons@gmail.com>
2018-08-18 17:43:46 -0500

    Update service worker link

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var markers = []
  */
  if ('serviceWorker' in navigator) {
    window.addEventListener('load', () => {
-     navigator.serviceWorker.register('../sw.js')
+     navigator.serviceWorker.register('./sw.js')
      .then(reg => console.log('Service Worker Registered'))
      .catch(err => console.log(`Service Worker Error: ${err}`));
    });
