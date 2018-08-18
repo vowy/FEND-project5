a3b8758f01d20cbfcd27d3d2030c6651751a0189
Eric Parsons <e.b.parsons@gmail.com>
2018-08-18 00:10:16 -0500

    Changed image URLs

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`fend-project5/img/${restaurant.photograph}`);
   }
 
   /**
